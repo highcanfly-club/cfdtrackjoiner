4907e9b58af04d6ec2f73d56c599e9034e27a000
Ronan LE MEILLAT <ronan.le_meillat@sctg.eu.org>
2022-02-04 19:33:03 +0100

    Working z-index

--- src/views/TrackJoinerView.vue
+++ src/views/TrackJoinerView.vue
@@ -276,8 +276,8 @@
   </div>
   <!-- Aide Slider -->
     <TransitionRoot as="template" :show="slideOpen">
-    <Dialog as="div" class="fixed inset-0 overflow-hidden" @close="slideOpen = false">
-      <div class="absolute inset-0 overflow-hidden z-50">
+    <Dialog as="div" class="fixed inset-0 overflow-hidden z-50" @close="slideOpen = false">
+      <div class="absolute inset-0 overflow-hidden">
         <TransitionChild as="template" enter="ease-in-out duration-500" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in-out duration-500" leave-from="opacity-100" leave-to="opacity-0">
           <DialogOverlay class="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
         </TransitionChild>
