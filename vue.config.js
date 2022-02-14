5d4bb51f8703fa1fcedf222dc16529614fe83188
Ronan LE MEILLAT <ronan.le_meillat@sctg.eu.org>
2022-02-14 16:44:19 +0100

    show build date in copyright

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,28 @@
+// vue.config.js
+const gitlog = require("gitlog").default;
+const fs = require('fs');
+
+// Option 1: Just use the function, returned commit type has specified fields
+const commits = gitlog({
+  repo: ".",
+  number: 1,
+  fields: ["authorDate"],
+});
+
+process.env.VUE_APP_GIT_TRACKJOINER_LAST_COMMIT = new Date(commits[0].authorDate);
+
+fs.writeFile('./commit.json', JSON.stringify(
+  { cfdtrackjoiner: (new Date(commits[0].authorDate)).toISOString(), 
+  }), 
+  'utf8', function (err) {
+                            if (err) return console.log(err);
+                          }
+           );
+
 module.exports = {
   runtimeCompiler: true,
   configureWebpack: {
-    devtool: 'source-map',
+    devtool: process.env.CF_PAGES==='1'?false:'eval-source-map',
     resolve: {
       fallback: {
         "fs": false,
